46b30af9201841052d837881294159cc93eda791
wqq524220257@qq.com <qwer1234>
2018-04-28 00:24:01 +0800

    crawler2

--- helloword/spider/tianyi/main.go
+++ helloword/spider/tianyi/main.go
@@ -101,14 +101,108 @@ func main(){
 	//	fmt.Println(q)
 	//}
 
-	resp, err := http.Get("http://em.ttyule6.com/api/web-ajax/is-login")
-	if err != nil {
-		// handle error
-	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
-	header := resp.Header
-	fmt.Println(header)
+	//resp, err := http.Get("http://em.ttyule6.com/api/web-ajax/is-login")
+	//if err != nil {
+	//	// handle error
+	//}
+	//defer resp.Body.Close()
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Println(string(body))
+	//header := resp.Header
+	////fmt.Printf("%#v",header["Set-Cookie"])
+	//
+	////re := regexp.MustCompile(header["Set-Cookie"])
+	//fmt.Println(Between(header["Set-Cookie"][0], "SESSION=", "; Path=/; HttpOnly"))
+	//fmt.Println(Between(header["Set-Cookie"][1], "__jsluid=", "; max-age=31536000"))
+	//
+	//cookiestrs := "__jsluid="+Between(header["Set-Cookie"][0], "SESSION=", "; Path=/; HttpOnly")+"; SESSION="+Between(header["Set-Cookie"][1], "__jsluid=", "; max-age=31536000")
+	//fmt.Println(cookiestrs)
 
+	client := &http.Client{}
+	//post请求
+	postValues := url.Values{}
+	//curl 'http://ttyl.109705.com/api/web-login'
+	// -H 'Cookie: __jsluid=70204eee8c4fee5e2d3fccf12cf8b5f6; SESSION=2405865d-90db-4106-9f70-a11ad6b025ac'
+	// -H 'Origin: http://ttyl.109705.com'
+	// -H 'Accept-Encoding: gzip, deflate'
+	// -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8'
+	// -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+	// -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8'
+	// -H 'Accept: application/json, text/javascript, */*; q=0.01'
+	// -H 'Referer: http://ttyl.109705.com/login.html'
+	// -H 'X-Requested-With: XMLHttpRequest'
+	// -H 'Connection: keep-alive'
+	// --data 'username=wqq9898&password=qwer1234&securityCode=2902' --compressed
+
+	//postValues.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	////postValues.Add("Cookie", cookiestrs)
+	////postValues.Add("Cookie", "__jsluid=70204eee8c4fee5e2d3fccf12cf8b5f6; SESSION=2405865d-90db-4106-9f70-a11ad6b025ac")
+	//postValues.Add("Host", "ttyl.109705.com")
+	//postValues.Add("Origin", "http://ttyl.109705.com")
+	//postValues.Add("Referer", "http://ttyl.109705.com/login.html")
+	//postValues.Add("username", "wqq9898")
+	//postValues.Add("password", "qwer1234")
+	//postValues.Add("securityCode", "2654")
+	//
+	//resp2, err := client.PostForm("http://ttyl.109705.com/api/web-login", postValues)
+	//defer resp2.Body.Close()
+	//if err != nil {
+	//	fmt.Println(err.Error())
+	//}
+	//if resp2.StatusCode == 200 {
+	//	body, _ := ioutil.ReadAll(resp2.Body)
+	//	fmt.Println(string(body))
+	//}
+
+	//curl 'http://ttyl.109705.com/api/game-lottery/query-trend'
+	// -H 'Cookie: __jsluid=70204eee8c4fee5e2d3fccf12cf8b5f6; SESSION=6e691e43-558d-4918-97bc-a572ec0e68c5'
+	// -H 'Origin: http://ttyl.109705.com' -H 'Accept-Encoding: gzip, deflate'
+	// -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8'
+	// -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+	// -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: application/json, text/javascript, */*; q=0.01'
+	// -H 'Referer: http://ttyl.109705.com/game/lottery/trend.html?qqmin' -H 'X-Requested-With: XMLHttpRequest'
+	// -H 'Connection: keep-alive'
+	// --data 'name=qqmin&query=latest-30' --compressed
+
+	postValues.Add("Accept", "application/json, text/javascript, */*; q=0.01")
+	postValues.Add("Accept-Encoding", "gzip, deflate")
+	postValues.Add("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+	postValues.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36")
+	postValues.Add("Connection", "keep-alive")
+	postValues.Add("Content-Length", "26")
+	postValues.Add("X-Requested-With", "XMLHttpRequest")
+
+	postValues.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	//postValues.Add("Cookie", cookiestrs)
+	postValues.Add("Cookie", "__jsluid=70204eee8c4fee5e2d3fccf12cf8b5f6; SESSION=6e691e43-558d-4918-97bc-a572ec0e68c5")
+	postValues.Add("Host", "ttyl.109705.com")
+	postValues.Add("Origin", "http://ttyl.109705.com")
+	postValues.Add("Referer", "http://ttyl.109705.com/game/lottery/trend.html?qqmin")
+	postValues.Add("name", "qqmin")
+	postValues.Add("query", "latest-30")
+
+	resp2, err := client.PostForm("http://ttyl.109705.com/api/game-lottery/query-trend", postValues)
+	defer resp2.Body.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if resp2.StatusCode == 200 {
+		body, _ := ioutil.ReadAll(resp2.Body)
+		fmt.Println(string(body))
+	}
+
+
+}
+
+func Between(str, starting, ending string) string {
+	s := strings.Index(str, starting)
+	if s < 0 {
+		return ""
+	}
+	s += len(starting)
+	e := strings.Index(str[s:], ending)
+	if e < 0 {
+		return ""
+	}
+	return str[s : s+e]
 }
