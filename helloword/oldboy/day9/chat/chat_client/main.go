8bbc90b97fb0af7cd8add40e31d2f04ae3490f99
wqq <wqq524220257@qq.com>
2018-05-25 17:52:22 +0800

    learn

---
+++ helloword/oldboy/day9/chat/chat_client/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"learngo/learngo/helloword/oldboy/day9/chat/proto"
+	"encoding/json"
+	"encoding/binary"
+	"errors"
+)
+
+func readPackage(conn net.Conn) (msg proto.Messge, err error){
+	var buf [8192]byte
+	n, err := conn.Read(buf[0:4])
+	if n != 4{
+		err = errors.New("read header failed")
+		return
+	}
+
+	var packLen uint32
+	packLen = binary.BigEndian.Uint32(buf[0:4])
+	fmt.Printf("receive len:%d", packLen)
+	n, err = conn.Read(buf[0:packLen])
+	if n != int(packLen){
+		err = errors.New("read body failed")
+		return
+	}
+
+	fmt.Printf("receive data:%s\n", string(buf[0:packLen]))
+	err = json.Unmarshal(buf[0:packLen], &msg)
+	if err != nil{
+		fmt.Println("unmarshal failed, err:", err)
+	}
+	return
+}
+
+func register(conn net.Conn) (err error){
+	var msg proto.Messge
+	msg.Cmd = proto.UserRegister
+
+	var loginCmdRes proto.LoginCmdRes
+
+	err = json.Unmarshal([]byte(msg.Data), &loginCmdRes)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func login(conn net.Conn) (err error){
+	var msg proto.Messge
+	msg.Cmd = proto.UserLogin
+
+	var loginCmd proto.LoginCmd
+	loginCmd.Id = 1
+	loginCmd.Passwd = "12345678"
+
+	data, err := json.Marshal(loginCmd)
+	if err != nil{
+		return
+	}
+
+	msg.Data = string(data)
+	data, err = json.Marshal(msg)
+	if err != nil{
+		return
+	}
+
+	var buf [4]byte
+	packLen := uint32(len(data))
+
+	fmt.Println("packlen:", packLen)
+	binary.BigEndian.PutUint32(buf[0:4], packLen)
+
+	n, err := conn.Write(buf[:])
+	if err != nil || n != 4{
+		fmt.Println("write data failed")
+		return
+	}
+
+	_, err = conn.Write([]byte(data))
+	if err != nil{
+		return
+	}
+
+	msg, err = readPackage(conn)
+	if err != nil{
+		fmt.Println("read package failed, err:", err)
+	}
+	fmt.Println(msg)
+	return
+}
+
+func main(){
+	conn, err := net.Dial("tcp", "localhost:10000")
+	if err != nil{
+		fmt.Println("Error dialing", err.Error())
+		return
+	}
+
+	err = login(conn)
+	if err != nil{
+		fmt.Println("login failed, err:", err)
+	}
+}
